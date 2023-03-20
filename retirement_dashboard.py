d9d293bec6bf8fe50e74b52cecb7bc27d532a924
Martine ZHANG <122970558+martinezhang@users.noreply.github.com>
2023-03-20 23:25:18 +0100

    Update retirement_dashboard.py

--- retirement_dashboard.py
+++ retirement_dashboard.py
@@ -5,8 +5,10 @@ import streamlit as st
 url = "https://www.data.gouv.fr/fr/datasets/r/83067d1a-a776-479f-9839-70e5ec5549a4"
 data = pd.read_csv(url, sep=";")
 
-# Définition de l'année
-annee = data.columns[0]
+# Extraire la première colonne comme série et prendre la première valeur comme année
+annee = data.iloc[:, 0][0]
+
+# Changer l'année de string en integer
 annee = int(annee)
 
 # Création du dashboard
@@ -15,7 +17,10 @@ categories = data["categorie_socioprofessionnelle"].unique()
 selected_category = st.sidebar.selectbox("Sélectionnez une catégorie socioprofessionnelle", categories)
 
 # Filtre les données selon la catégorie sélectionnée
-filtered_data = data[(data["categorie_socioprofessionnelle"] == selected_category) & (data[annee] == selected_year)]
+filtered_data = data[data["categorie_socioprofessionnelle"] == selected_category]
+
+# Filtrer les données selon l'année sélectionnée
+filtered_data = filtered_data[filtered_data["annee"] == annee]
 
 # Affichage des données
 st.write("Données pour la catégorie socioprofessionnelle :", selected_category)
@@ -24,4 +29,4 @@ st.write(filtered_data)
 # Affichage de graphiques
 st.subheader("Graphiques")
 st.bar_chart(filtered_data[["age_conjoncturel_de_depart_a_la_retraite", "duree_moyenne_en_emploi_hors_cumul", "duree_moyenne_sans_emploi_ni_retraite"]])
-st.line_chart(filtered_data[["proportion_de_retraites_a_61_ans", "proportion_de_personnes_fortement_limitees_au_cours_de_la_premiere_annee_de_retraite", "proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite"]])
+st.line_chart(filtered_data[["proportion_de_retraites_a_61_ans", "proportion_de_personnes_fortement_limitees_au_cours_de_la_premiere_annee_de_retraite", "proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite"]]) 
