79c84b4019f3041dfc5f108aa8c0877dc36f0bf4
Martine ZHANG <122970558+martinezhang@users.noreply.github.com>
2023-03-20 23:40:20 +0100

    Update retirement_dashboard.py

--- retirement_dashboard.py
+++ retirement_dashboard.py
@@ -5,11 +5,9 @@ import streamlit as st
 url = "https://www.data.gouv.fr/fr/datasets/r/83067d1a-a776-479f-9839-70e5ec5549a4"
 data = pd.read_csv(url, sep=";")
 
-# Extraire la première colonne comme série et prendre la première valeur comme année
-annee = data.iloc[:, 0][0]
-
-# Changer l'année de string en integer
-annee = int(annee)
+# Transformer la première colonne en entiers et prendre toutes les valeurs de la colonne comme années
+data.iloc[:, 0] = data.iloc[:, 0].astype(int)
+annees = data.iloc[:, 0].unique()
 
 # Création du dashboard
 st.title("Age de départ à la retraite et conditions de fin de carrière selon la catégorie socioprofessionnelle")
@@ -20,10 +18,11 @@ selected_category = st.sidebar.selectbox("Sélectionnez une catégorie socioprof
 filtered_data = data[data["categorie_socioprofessionnelle"] == selected_category]
 
 # Filtrer les données selon l'année sélectionnée
-filtered_data = filtered_data[filtered_data["annee"].astype(int) == annee]
+selected_year = st.sidebar.selectbox("Sélectionnez une année", annees)
+filtered_data = filtered_data[filtered_data["annee"] == selected_year]
 
 # Affichage des données
-st.write("Données pour la catégorie socioprofessionnelle :", selected_category)
+st.write("Données pour la catégorie socioprofessionnelle :", selected_category, "et l'année", selected_year)
 st.write(filtered_data)
 
 # Affichage de graphiques
