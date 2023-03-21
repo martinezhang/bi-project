260ad64cdbf1e602d5d47eb2b4b945822ca6e5e1
Martine ZHANG <122970558+martinezhang@users.noreply.github.com>
2023-03-22 00:09:43 +0100

    Update retirement_dashboard.py

--- retirement_dashboard.py
+++ retirement_dashboard.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 
 # Chargement des données
 url = "https://www.data.gouv.fr/fr/datasets/r/83067d1a-a776-479f-9839-70e5ec5549a4"
@@ -19,10 +18,9 @@ st.title("Age de départ à la retraite et conditions de fin de carrière selon 
 # Filtres
 selected_category = st.sidebar.selectbox("Sélectionnez une catégorie socioprofessionnelle", sorted(data["categorie_socioprofessionnelle"].unique()))
 selected_year = st.sidebar.selectbox("Sélectionnez une année", annees)
-selected_region = st.sidebar.selectbox("Sélectionnez une région", sorted(data["region"].unique()))
 
 # Filtrage des données selon les filtres sélectionnés
-filtered_data = data[(data["categorie_socioprofessionnelle"] == selected_category) & (data["annee"] == selected_year) & (data["region"] == selected_region)]
+filtered_data = data[(data["categorie_socioprofessionnelle"] == selected_category) & (data["annee"] == selected_year)]
 
 # Affichage des données
 st.write("Données pour la catégorie socioprofessionnelle :", selected_category, "et l'année", selected_year)
