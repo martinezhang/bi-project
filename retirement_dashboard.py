f2ac768c2c9523ff6caa9d8c99865389f0221961
Martine ZHANG <122970558+martinezhang@users.noreply.github.com>
2023-03-21 23:58:01 +0100

    Update retirement_dashboard.py

--- retirement_dashboard.py
+++ retirement_dashboard.py
@@ -1,22 +1,32 @@
 import pandas as pd
-import plotly.express as px
+import streamlit as st
 
-# Charger les données
-data = pd.read_csv("donnees_retraite.csv")
+# Chargement des données
+url = "https://www.data.gouv.fr/fr/datasets/r/83067d1a-a776-479f-9839-70e5ec5549a4"
+data = pd.read_csv(url, sep=";")
 
-# Créer le dashboard
-dashboard = px.scatter(data, x="annee", y="proportion_de_personnes_fortement_limitees_au_cours_de_la_premiere_annee_de_retraite",
-                        color="categorie_socioprofessionnelle",
-                        hover_data=["proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite",
-                                    "age_conjoncturel_de_depart_a_la_retraite", "proportion_de_retraites_a_61_ans",
-                                    "duree_moyenne_en_emploi_hors_cumul", "duree_moyenne_sans_emploi_ni_retraite"])
+# Nettoyage des données
+data.iloc[:, 0] = data.iloc[:, 0].astype(int)  # conversion de la première colonne en entiers
+data.dropna(inplace=True)  # suppression des lignes contenant des valeurs manquantes
 
-# Appliquer les filtres
-dashboard.update_traces(visible="legendonly", selector=dict(name="proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite"))
-dashboard.update_traces(visible="legendonly", selector=dict(name="age_conjoncturel_de_depart_a_la_retraite"))
-dashboard.update_traces(visible="legendonly", selector=dict(name="proportion_de_retraites_a_61_ans"))
-dashboard.update_traces(visible="legendonly", selector=dict(name="duree_moyenne_en_emploi_hors_cumul"))
-dashboard.update_traces(visible="legendonly", selector=dict(name="duree_moyenne_sans_emploi_ni_retraite"))
+# Liste des années
+annees = sorted(data["annee"].unique())
 
-# Afficher le dashboard
-dashboard.show()
+# Création du dashboard
+st.title("Age de départ à la retraite et conditions de fin de carrière selon la catégorie socioprofessionnelle")
+
+# Filtres
+selected_category = st.sidebar.selectbox("Sélectionnez une catégorie socioprofessionnelle", sorted(data["categorie_socioprofessionnelle"].unique()))
+selected_year = st.sidebar.selectbox("Sélectionnez une année", annees)
+
+# Filtrage des données selon les filtres sélectionnés
+filtered_data = data[(data["categorie_socioprofessionnelle"] == selected_category) & (data["annee"] == selected_year)]
+
+# Affichage des données
+st.write("Données pour la catégorie socioprofessionnelle :", selected_category, "et l'année", selected_year)
+st.write(filtered_data)
+
+# Affichage de graphiques
+st.subheader("Graphiques")
+st.bar_chart(filtered_data[["age_conjoncturel_de_depart_a_la_retraite", "duree_moyenne_en_emploi_hors_cumul", "duree_moyenne_sans_emploi_ni_retraite"]])
+st.line_chart(filtered_data[["proportion_de_retraites_a_61_ans", "proportion_de_personnes_fortement_limitees_au_cours_de_la_premiere_annee_de_retraite", "proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite"]])
