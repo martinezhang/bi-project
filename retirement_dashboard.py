1ce104a59d1c56fe640eef777168cdb6794c8e02
Martine ZHANG <122970558+martinezhang@users.noreply.github.com>
2023-03-21 23:51:30 +0100

    Update retirement_dashboard.py

--- retirement_dashboard.py
+++ retirement_dashboard.py
@@ -3,24 +3,30 @@ import streamlit as st
 
 # Chargement des données
 url = "https://www.data.gouv.fr/fr/datasets/r/83067d1a-a776-479f-9839-70e5ec5549a4"
-df = pd.read_csv(url, sep=";")
+data = pd.read_csv(url, sep=";")
 
-# Renommer les colonnes
-df = df.rename(columns={
-    "proportion_de_personnes_fortement_limitees_au_cours_de_la_premiere_annee_de_retraite": "fortement_limite",
-    "proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite": "limite",
-    "age_conjoncturel_de_depart_a_la_retraite": "age_depart",
-    "proportion_de_retraites_a_61_ans": "retraite_61",
-    "duree_moyenne_en_emploi_hors_cumul": "duree_emploi",
-    "duree_moyenne_sans_emploi_ni_retraite": "duree_chomage"
-})
+# Nettoyage des données
+data.iloc[:, 0] = data.iloc[:, 0].astype(int)  # conversion de la première colonne en entiers
+data.dropna(inplace=True)  # suppression des lignes contenant des valeurs manquantes
 
-# Supprimer les colonnes inutiles
-df = df.drop(["mois", "sexe"], axis=1)
+# Liste des années
+annees = sorted(data["annee"].unique())
 
-# Convertir la colonne annee en type datetime
-df["annee"] = pd.to_datetime(df["annee"], format="%Y")
+# Création du dashboard
+st.title("Age de départ à la retraite et conditions de fin de carrière selon la catégorie socioprofessionnelle")
 
-# Créer le tableau de bord avec Streamlit
-st.title("Tableau de bord de la base de données des retraites")
-st.dataframe(df)
+# Filtres
+selected_category = st.sidebar.selectbox("Sélectionnez une catégorie socioprofessionnelle", sorted(data["categorie_socioprofessionnelle"].unique()))
+selected_year = st.sidebar.selectbox("Sélectionnez une année", annees)
+
+# Filtrage des données selon les filtres sélectionnés
+filtered_data = data[(data["categorie_socioprofessionnelle"] == selected_category) & (data["annee"] == selected_year)]
+
+# Affichage des données
+st.write("Données pour la catégorie socioprofessionnelle :", selected_category, "et l'année", selected_year)
+st.write(filtered_data)
+
+# Affichage de graphiques
+st.subheader("Graphiques")
+st.bar_chart(filtered_data[["age_conjoncturel_de_depart_a_la_retraite", "duree_moyenne_en_emploi_hors_cumul", "duree_moyenne_sans_emploi_ni_retraite"]])
+st.line_chart(filtered_data[["proportion_de_retraites_a_61_ans", "proportion_de_personnes_fortement_limitees_au_cours_de_la_premiere_annee_de_retraite", "proportion_de_personnes_limitees_mais_pas_fortement_au_cours_de_la_premiere_annee_de_retraite"]])
