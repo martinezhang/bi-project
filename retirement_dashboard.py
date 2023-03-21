407de0ea87abd9622d00b5274c5be372f5c3fbfe
Martine ZHANG <122970558+martinezhang@users.noreply.github.com>
2023-03-22 00:48:20 +0100

    Update retirement_dashboard.py

--- retirement_dashboard.py
+++ retirement_dashboard.py
@@ -19,8 +19,8 @@ st.title("Age de départ à la retraite et conditions de fin de carrière selon 
 # Filtres
 selected_category = st.sidebar.selectbox("Sélectionnez une catégorie socioprofessionnelle", sorted(data["categorie_socioprofessionnelle"].unique()))
 selected_year = st.sidebar.selectbox("Sélectionnez une année", annees)
-selected_min_proportion_fortement_limitees = st.sidebar.slider("Sélectionnez la proportion minimale de personnes fortement limitées au cours de la première année de retraite", 0, 100, 0, 5)
-selected_max_proportion_fortement_limitees = st.sidebar.slider("Sélectionnez la proportion maximale de personnes fortement limitées au cours de la première année de retraite", 0, 100, 100, 5)
+--selected_min_proportion_fortement_limitees = st.sidebar.slider("Sélectionnez la proportion minimale de personnes fortement limitées au cours de la première année de retraite", 0, 100, 0, 5)
+--selected_max_proportion_fortement_limitees = st.sidebar.slider("Sélectionnez la proportion maximale de personnes fortement limitées au cours de la première année de retraite", 0, 100, 100, 5)
 selected_min_proportion_limitees_mais_pas_fortement = st.sidebar.slider("Sélectionnez la proportion minimale de personnes limitées mais pas fortement au cours de la première année de retraite", 0, 100, 0, 5)
 selected_max_proportion_limitees_mais_pas_fortement = st.sidebar.slider("Sélectionnez la proportion maximale de personnes limitées mais pas fortement au cours de la première année de retraite", 0, 100, 100, 5)
 selected_age_conjoncturel = st.sidebar.multiselect("Sélectionnez les tranches d'âge pour l'âge conjoncturel de départ à la retraite", sorted(data["age_conjoncturel_de_depart_a_la_retraite"].unique()))
@@ -31,7 +31,7 @@ selected_duree_moyenne_sans_emploi_ni_retraite = st.sidebar.multiselect("Sélect
 
 # Filtrage des données selon les filtres sélectionnés
 filtered_data = data[(data["categorie_socioprofessionnelle"] == selected_category) & (data["annee"] == selected_year)]
-filtered_data = filtered_data[(filtered_data["proportion_fortement_limitees"] >= selected_min_proportion_fortement_limitees/100) & (filtered_data["proportion_fortement_limitees"] <= selected_max_proportion_fortement_limitees/100)]
+--filtered_data = filtered_data[(filtered_data["proportion_fortement_limitees"] >= selected_min_proportion_fortement_limitees/100) & (filtered_data["proportion_fortement_limitees"] <= selected_max_proportion_fortement_limitees/100)]
 filtered_data = filtered_data[(filtered_data["proportion_limitees_mais_pas_fortement"] >= selected_min_proportion_limitees_mais_pas_fortement/100) & (filtered_data["proportion_limitees_mais_pas_fortement"] <= selected_max_proportion_limitees_mais_pas_fortement/100)]
 filtered_data = filtered_data[filtered_data["age_conjoncturel_de_depart_a_la_retraite"].isin(selected_age_conjoncturel)]
 filtered_data = filtered_data[(filtered_data["proportion_retraites_a_61_ans"] >= selected_min_proportion_retraites/100) & (filtered_data["proportion_retraites_a_61_ans"] <= selected_max_proportion_retraites/100)]
@@ -39,6 +39,7 @@ filtered_data = filtered_data[filtered_data["duree_moyenne_en_emploi_hors_cumul"
 filtered_data = filtered_data[filtered_data["duree_moyenne_sans_emploi_ni_retraite"].isin(selected_duree_moyenne_sans_emploi_ni_retraite)]
 
 # Affichage des données
+st.write("Données pour la catégorie socioprofessionnelle :", selected_category, "et l'année", selected_year)
 st.write(filtered_data)
 
 # Affichage de graphiques
